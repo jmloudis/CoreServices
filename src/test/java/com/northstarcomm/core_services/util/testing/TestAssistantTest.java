ecd15db9e6289082f4c7e72ca4da8ff3c273b6ca
jordan <jordan@jordans-MacBook-Pro.local>
2023-05-14 10:03:52 -0400

    Added unit tests and updated group directory

---
+++ src/test/java/com/northstarcomm/core_services/util/testing/TestAssistantTest.java
@@ -0,0 +1,27 @@
+package com.northstarcomm.core_services.util.testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Properties;
+
+import static com.northstarcomm.core_services.util.testing.TestAssistant.readPropertiesFile;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestAssistantTest {
+
+
+    @Test
+    public void testReadPropertiesFileSuccess() {
+        Properties properties = readPropertiesFile("test-data.properties");
+        String testValue = "bar";
+        String testKey = "foo";
+
+        assertEquals(testKey, properties.getProperty("key"));
+        assertEquals(testValue, properties.getProperty("value"));
+    }
+
+
+}
